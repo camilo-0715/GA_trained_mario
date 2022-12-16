27698646b59c4221d25ca76b6c106b9d4066458a
camilo_0715 <jc.carmona.2019@alumnos.urjc.es>
2022-12-16 19:58:43 +0100

    ya vaaaaaaaaaaa

--- data/genetic_algorithm_mario.py
+++ data/genetic_algorithm_mario.py
@@ -14,17 +14,19 @@ def fitness_func(solution, solution_idx):
 
     #solution deberia ser una lista de movimientos  
     global run_it
-
+    points = 0
     
     keyboard = Controller()
     moves = []
 
-    #print(solution)
+    min_jump_prob = 1.9
+    max_jump_prob = 2.0
+
     for i in solution:
-        if i >= 0.0 and  i < 1.0:
-            moves.append(Key.right)           
+        if i >= 0.0 and  i < 1.9:
+            moves.append(Key.right)
            
-        elif i >= 1.0 and i <= 2.0:
+        elif i >= min_jump_prob and i <= max_jump_prob:
             moves.append('a')           
 
     counter = 0
@@ -41,23 +43,29 @@ def fitness_func(solution, solution_idx):
         counter = counter + 1
 
         if (run_it.state.game_info[c.MARIO_DEAD]):
+            points = points - 100
             break
 
 
-    return run_it.state.mario.rect.x / 8751
+    for i in range(counter):
+        if solution[i] >= 0.0 and  solution[i] < 1.9:
+            points = points + 2           
+        
+        elif solution[i] >= min_jump_prob and solution[i] <= max_jump_prob:
+            points = points + 1
+
+
+    if run_it.state.mario.rect.x >= 8749:
+        points = points + 100000
+
+    print("individual points: ", points )
+    return points
 
 def callback_generation(ga_instance):
-    global last_fitness
     
-    best_sol_fitness = ga_instance.best_solution()[1]
-    fitness_change = best_sol_fitness - last_fitness
-
-    last_fitness = best_sol_fitness
-
-
+    #best_sol_fitness = ga_instance.best_solution()[1]
+    
     print("Generation  = {generation}".format(generation=ga_instance.generations_completed))
-    print("Fitness     = {fitness}".format(fitness=ga_instance.best_solution()[1]))
-    print("Change     = {change}".format(change=fitness_change))
 
 
 #    print("Generation  = {generation}".format(generation=ga_instance.generations_completed))
@@ -75,17 +83,19 @@ def main():
     run_it.setup_states(state_dict, c.MAIN_MENU)
 
 
-    ga_instance = pygad.GA(num_generations=10,
-                            num_parents_mating=1, 
-                            fitness_func=fitness_func,
-                            sol_per_pop=100, 
-                            num_genes=10000,
-                            init_range_low=0.0,
-                            init_range_high=3.0,
-                            random_mutation_min_val=0.0,
-                            random_mutation_max_val=2.0,
-                            callback_generation=callback_generation,
-                            mutation_by_replacement=True,
-                            delay_after_gen=1)
+
+    ga_instance = pygad.GA(num_generations=100,
+                               num_parents_mating=1,
+                               mutation_type='random', 
+                               mutation_probability=0.1,
+                               fitness_func=fitness_func,
+                               sol_per_pop=3, 
+                               num_genes=10000,
+                               init_range_low=0.0,
+                               init_range_high=2.0,
+                               random_mutation_min_val=0.0,
+                               random_mutation_max_val=3.0,
+                               mutation_by_replacement=True,
+                               callback_generation=callback_generation)
 
     ga_instance.run()
