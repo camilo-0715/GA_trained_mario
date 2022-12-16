044658bc8412c1e1205006a0e50ac67ce5ebee2b
Chuismiguel <luismilopez35@gmail.com>
2022-12-16 21:14:44 +0100

    scrambñe + points

--- data/genetic_algorithm_mario.py
+++ data/genetic_algorithm_mario.py
@@ -10,7 +10,7 @@ import numpy as np
 run_it = tools.Control(setup.ORIGINAL_CAPTION)
 
 
-def fitness_func(solution, solution_idx):
+def mario_fitness(solution, solution_idx):
 
     #solution deberia ser una lista de movimientos  
     global run_it
@@ -19,11 +19,11 @@ def fitness_func(solution, solution_idx):
     keyboard = Controller()
     moves = []
 
-    min_jump_prob = 1.9
+    min_jump_prob = 1.8
     max_jump_prob = 2.0
 
     for i in solution:
-        if i >= 0.0 and  i < 1.9:
+        if i >= 0.0 and  i < min_jump_prob:
             moves.append(Key.right)
            
         elif i >= min_jump_prob and i <= max_jump_prob:
@@ -37,7 +37,7 @@ def fitness_func(solution, solution_idx):
 
         run_it.main()
 
-        if counter < len(moves):        
+        if counter < len(moves):                
             keyboard.release(moves[counter])
         
         counter = counter + 1
@@ -47,17 +47,13 @@ def fitness_func(solution, solution_idx):
             break
 
 
-    for i in range(counter):
-        if solution[i] >= 0.0 and  solution[i] < 1.9:
-            points = points + 2           
-        
-        elif solution[i] >= min_jump_prob and solution[i] <= max_jump_prob:
-            points = points + 1
-
-
+    points = run_it.state.mario.rect.x + run_it.state.game_info[c.SCORE]
+    
+    run_it.state.game_info[c.SCORE] = 0
     if run_it.state.mario.rect.x >= 8749:
         points = points + 100000
 
+    run_it.main()
     print("individual points: ", points )
     return points
 
@@ -86,16 +82,16 @@ def main():
 
     ga_instance = pygad.GA(num_generations=100,
                                num_parents_mating=1,
-                               mutation_type='random', 
-                               mutation_probability=0.1,
-                               fitness_func=fitness_func,
+                               mutation_type='scramble', 
+                               mutation_probability=0.25,
+                               fitness_func=mario_fitness,
                                sol_per_pop=3, 
                                num_genes=10000,
                                init_range_low=0.0,
                                init_range_high=2.0,
                                random_mutation_min_val=0.0,
                                random_mutation_max_val=3.0,
-                               mutation_by_replacement=True,
+                               mutation_by_replacement=False,
                                callback_generation=callback_generation)
 
     ga_instance.run()
